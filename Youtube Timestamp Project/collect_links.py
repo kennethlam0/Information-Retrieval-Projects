a2d949f5421034377d9d81e79dc3095223baf492
Kenneth Lam <kennethlam0812@gmail.com>
2024-06-26 12:27:43 -0400

    Delete Youtube Timestamp Project directory

--- Youtube Timestamp Project/collect_links.py
+++
@@ -1,8 +0,0 @@
-import json
-
-links_database = {}
-with open('youtube-transcriptions.jsonl', 'r') as f:
-    for line in f:
-        line = json.loads(line)
-        links_database[line['title'].encode('utf-8')] = line['url']
-print(links_database)
